fe1c41f0af901d2552c072d0bed9eb047c86a4c8
ChrM3r <chris.merscher@beuth-hochschule.de>
2018-05-01 01:49:46 +0200

    Arebiten an ESA ALE_E2

--- ESA_ALE/ALE_2/MeineArrayList.java
+++ ESA_ALE/ALE_2/MeineArrayList.java
@@ -24,8 +24,51 @@ public class MeineArrayList<E> {
         increment = inc;
 
     }
+    protected boolean add(E e){
 
-    public String toString(MeineArrayList meineAL){
-        return Arrays.toString(meineAL);
+        E[] tempArray = Arrays.copyOf(this.list, this.size + 1);
+        tempArray[size] = e;
+        this.list = Arrays.copyOf(tempArray, this.size + 1);
+
+        this.size++;
+        if (this.capacity < this.size) this.capacity++;
+
+        return true;
+    }
+
+    protected boolean remove(int index){
+
+        boolean rue = false;
+
+        if (!(index > list.length - 1 && index < 0 ) && size > 0){
+            E[] tempArray = (E[]) new Object[size - 1];
+            System.arraycopy(list, index, tempArray,index - 1, capacity - 1);
+            for (int i = 0; i < index; i++) {
+                System.arraycopy(list, i, tempArray,i, size - 1);
+            }
+
+            for (int i = index + 1; i < tempArray.length; i++){
+                System.arraycopy(list, index + 1, tempArray, index, size - 1);
+            }
+            list = Arrays.copyOf(tempArray, size - 1);
+            size--;
+            rue = true;
+        }
+        return rue;
+    }
+    protected void clear(){
+
+        E[] tempArray = (E[]) new Object[0];
+        list = tempArray;
+    }
+    protected int size(){
+
+        int zaehler = 0;
+        for (E e: list) zaehler++;
+        return zaehler;
+    }
+
+    public String toString(){
+        return Arrays.toString(list);
     }
 }
---
+++ test/ESA_ALE/ALE_2/MeineArrayListTest.java
@@ -0,0 +1,66 @@
+package ESA_ALE.ALE_2;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MeineArrayListTest {
+
+    private MeineArrayList meineAL = new MeineArrayList(0, 0);
+
+  //  @AfterEach
+  //  public void clearList() {
+  //      meineAL.clear();
+  //  }
+
+
+@Nested
+    @DisplayName("Typ int")
+    class MeineArrayListTestTypInt {
+
+        @Test
+        public void testAdd() {
+
+
+            meineAL.add(1);
+            meineAL.add(2);
+            meineAL.add(3);
+            System.out.println(meineAL);
+
+
+            assertEquals(3, meineAL.size());
+        }
+
+        @Test
+        public void testRemove() {
+            assertTrue(meineAL.remove(1));
+
+        }
+
+        @Test
+        public void testClear() {
+            meineAL.clear();
+            assertEquals(0, meineAL.size());
+        }
+
+        @Test
+        public void TestToString() {
+            meineAL.add(1);
+            meineAL.add(2);
+            meineAL.add(3);
+
+            assertEquals("[1, 2, 3]", meineAL.toString());
+        }
+
+    }
+
+}
